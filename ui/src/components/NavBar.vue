67c1d7b581a0d2be96d4ac70b2deac5276892c64
Arial Gardner <arialgardner@gmail.com>
2025-10-17 14:37:48 -0400

    update name & tab icon

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -6,7 +6,7 @@
         <button class="back-button" @click="handleBackToDashboard" title="Back to Dashboard">
           ← Back
         </button>
-        <h1>CollabCanvas</h1>
+        <h1>Collab Canvas</h1>
       </div>
 
       <!-- User Info & Controls -->
